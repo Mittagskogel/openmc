2c368bfe359e656eb5c134d18533a8cfa4d273b2
Faveo Hörold <faveo.hoerold@cscs.ch>
2024-09-24 10:16:04 +0200

    Update main.cpp and CMakeLists.txt to truncate fp.

--- src/main.cpp
+++ src/main.cpp
@@ -9,24 +9,18 @@
 #include "openmc/random_ray/random_ray_simulation.h"
 #include "openmc/settings.h"
 
-int main(int argc, char* argv[])
-{
-  using namespace openmc;
-  int err;
 
-  // Initialize run -- when run with MPI, pass communicator
-#ifdef OPENMC_MPI
-  MPI_Comm world {MPI_COMM_WORLD};
-  err = openmc_init(argc, argv, &world);
-#else
-  err = openmc_init(argc, argv, nullptr);
-#endif
-  if (err == -1) {
-    // This happens for the -h and -v flags
-    return 0;
-  } else if (err) {
-    fatal_error(openmc_err_msg);
-  }
+// Enzyme
+#include "enzyme/fprt/fprt.h"
+#define FROM 16
+#define TO 8
+
+template <typename fty> fty *__enzyme_truncate_mem_func(fty *, int, int);
+template <typename fty> fty *__enzyme_truncate_op_func(fty *, int, int, int);
+
+
+void main_enzyme(int argc, char* argv[], int err) {
+  using namespace openmc;
 
   // start problem based on mode
   switch (settings::run_mode) {
@@ -63,6 +57,28 @@ int main(int argc, char* argv[])
   err = openmc_finalize();
   if (err)
     fatal_error(openmc_err_msg);
+}
+
+int main(int argc, char* argv[])
+{
+  using namespace openmc;
+  int err;
+
+  // Initialize run -- when run with MPI, pass communicator
+#ifdef OPENMC_MPI
+  MPI_Comm world {MPI_COMM_WORLD};
+  err = openmc_init(argc, argv, &world);
+#else
+  err = openmc_init(argc, argv, nullptr);
+#endif
+  if (err == -1) {
+    // This happens for the -h and -v flags
+    return 0;
+  } else if (err) {
+    fatal_error(openmc_err_msg);
+  }
+
+  __enzyme_truncate_op_func(main_enzyme, FROM, 0, TO)(argc, argv, err);
 
     // If MPI is in use and enabled, terminate it
 #ifdef OPENMC_MPI
